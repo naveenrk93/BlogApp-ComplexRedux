37d4d098436e1433f07614535786d338440c0478
Naveen <naveenrk93@gmail.com>
2018-06-01 16:47:24 -0700

    New Post Adding Capabilities added

---
+++ src/actions/index.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api/';
+const API_KEY = '?key=Naveen1234';
+
+export function FetchPosts(){
+
+    const request = axios.get(`${ROOT_URL}posts${API_KEY}`);
+    return{
+        type: FETCH_POSTS,
+        payload: request
+    }
+}
+
+export function createPost(values, callback){
+    const request = axios.post(`${ROOT_URL}posts${API_KEY}`, values)
+        .then( () => callback() );
+
+    return{
+        type: CREATE_POST,
+        payload: request
+    }
+}
--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route , Switch} from 'react-router-dom';
+import ReduxPromise from 'redux-promise'
 
-import App from './components/app';
+import PostsIndex from './components/posts-index';
 import reducers from './reducers';
+import PostNew from './components/post-new';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+      <BrowserRouter>
+          <div>
+              <Route exact path="/posts/new" component={PostNew} />
+              <Route exact path="/" component={PostsIndex} />
+          </div>
+      </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
