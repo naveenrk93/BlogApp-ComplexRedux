fe0b184f492927dd6b550942408e4150909e9f1c
Naveen <naveenrk93@gmail.com>
2018-06-01 16:49:02 -0700

    Adding extra components

---
+++ src/components/posts-index.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import {FetchPosts} from './../actions';
+
+class PostsIndex extends Component{
+    componentDidMount(){
+        this.props.FetchPosts();
+    }
+
+    renderPosts(){
+        return(
+            _.map(this.props.posts, post => {
+                return <li className="list-group-item" key={post.id}>{post.title}</li>
+            })
+        );
+    }
+
+    render(){
+        return (
+            <div>
+                <div className="text-xs-right">
+                    <Link className="btn btn-primary" to="/posts/new">
+                        Create a new Post
+                    </Link>
+                </div>
+                <h3>POSTS</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state){
+    return {posts: state.posts};
+}
+export default  connect(mapStateToProps, {FetchPosts})(PostsIndex);
